d9e72c325d550d9fa66f16b2bf9194d1a1f34da3
Ali Elshorbagy <a.ashorbagy@dev.thiqah.sa>
2021-01-11 10:50:33 +0300

    Implement Repository and Services

---
+++ CleanArch/CleanArch.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infrastructure.Data.Context;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanArch.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            this._context = universityDBContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return this._context.Courses.ToList();
+        }
+    }
+}
