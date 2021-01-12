497c4e190c1fa8efe139e6fa23c8cf4b2e5d071e
Ali Elshorbagy <a.ashorbagy@dev.thiqah.sa>
2021-01-12 11:59:55 +0300

    WebApi Configuration options setup

--- CleanArch/CleanArch.WebApi/Startup.cs
+++ CleanArch/CleanArch.WebApi/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArch.Infrastructure.Data.Context;
+using CleanArch.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CleanArch.WebApi
 {
@@ -25,7 +21,13 @@ namespace CleanArch.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UniversityDBContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityDBConnection")));
+
             services.AddControllers();
+
+            services.RegisterDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
